edd18818d6adec3d2123385226287bf854c47fdb
Bhuwan Bhurtel <astrofic7@gmail.com>
2024-02-06 20:02:43 +0545

    #30.2 AdminInfo

--- src/components/admin/adminProfile/AdminProfile.tsx
+++ src/components/admin/adminProfile/AdminProfile.tsx
@@ -6,6 +6,9 @@ import AdminMenu from "../components/adminMenu/AdminMenu";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import EditAdminForm from "./EditAdminForm";
+import {getProfile} from "@/hook/useProfile";
+import { UserButton, SignOutButton } from "@clerk/nextjs";
+
 
 interface AdminPageProps {
     
@@ -20,6 +23,11 @@ const AdminProfile: FunctionComponent<AdminPageProps> = () => {
         number: '',
         restaurant: '',
     })
+
+    const { data: profileData }: any = getProfile();
+    
+    const userName = profileData?.data?.message?.firstName;
+    const userEmail = profileData?.data?.message?.emailAddresses[0]?.emailAddress;
 
     const toggleForm = () => {
         setEditProfile(!editProfile)
@@ -40,24 +48,15 @@ const AdminProfile: FunctionComponent<AdminPageProps> = () => {
                 </div>
                 {/* Admin Items */}
                 <div className='flex-[11_11_0%]'>
-                    {!editProfile ? <div className="h-screen flex flex-col justify-center items-center pt-20 md:pt-0">
-                    <div className={`bg-[#41ad4110] text-[40px] rounded-[50%] p-5 flex items-center`} >
-                        🍗
+                     <div className="h-screen flex flex-col justify-center items-center pt-20 md:pt-0">
+                    <div className={`bg-[#41ad4110] text-[40px] scale-150 rounded-[50%] p-2 flex items-center`} >
+                        <UserButton afterSignOutUrl="/"/>
                     </div>
-                    <h1 className="font-semibold text-4xl md:text-3xl text-adminblueColor mt-5">{formData.name == ''? 'Your Name' : formData.name}</h1>
-                    <p className="text-gray-400 text-xl md:text-sm mt-2">{formData.name == ''? 'Your Restaurant Name' : formData.restaurant}</p>
-                    <div className="flex justify-between md:text-[12px] mt-4 text-gray-500 items-center gap-1">
-                        <FaLocationDot />
-                        <p>{formData.name == ''? 'Your Location' : formData.location}</p>
+                    <h1 className="font-semibold text-4xl md:text-3xl text-adminblueColor mt-5">{userName}</h1>
+                    <p className="text-gray-400 text-xl md:text-sm mt-2">{userEmail}</p>
+                    
+                    
                     </div>
-                    <div className="md:text-[12px] text-gray-500 mt-1">{formData.name == ''? 'Your phone no' : formData.number}</div>
-                    <div className="flex flex-col items-center  md:flex-row px-5 py-5 gap-5 mt-5">
-                        <button onClick={() => {setEditProfile(true)}} className="transition-all hover:scale-[1.05]  bg-transparent hover:bg-adminblueColor text-[12px] border-[1px] border-adminblueColor py-2 px-5 text-black font-bold">Edit Profile</button>
-                        <Link href={'/login'}>
-                        <button className="transition-all  bg-adminblueColor  font-semibold hover:shadow hover:scale-[1.05] text-[14px] border-[1px] border-adminblueColor py-2 px-5 text-gray-900">Logout</button>
-                        </Link>
-                    </div>
-                    </div> : <EditAdminForm toggleForm={toggleForm} formDatas={formDatas} />}
                 </div>
                 
                 <div></div>
