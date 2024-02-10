66f1fffbbd8d8db3ea79904e41da52288d7b9e4b
Bhuwan Bhurtel <astrofic7@gmail.com>
2024-02-10 19:19:50 +0545

    #42.2 todo add herosection

--- src/components/orderFood/OrderFoodComponent.tsx
+++ src/components/orderFood/OrderFoodComponent.tsx
@@ -2,6 +2,7 @@ import MethodHeader from '@/components/component/Header/MethodHeader'
 import HorizontalMenu from '@/components/Menu/horizontalMenu/HorizontalMenu'
 import VerticalMenu from '@/components/Menu/verticalMenu/VerticalMenu'
 import Search from '@/components/component/search/Search'
+import HeroSection from '../component/HeroSection'
 
 const OrderFoodComponent = () => {
   return (
@@ -13,6 +14,9 @@ const OrderFoodComponent = () => {
         <div className='min-h-[10vh]'></div>
 
         {/* Hero-Section */}
+        <div className=''>
+          <HeroSection />
+        </div>
 
         {/* Search */}
         <div className='mx-5 flex  justify-center '>
