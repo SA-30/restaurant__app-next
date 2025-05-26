f29d0afbcf52b6fe0ebea29ef86566e74f66f243
bhuwan bhurtel <astrofic7@gmail.com>
2025-05-26 18:23:44 +0545

    fix on may26 2025

--- src/lib/mongoConnect.js
+++ src/lib/mongoConnect.js
@@ -1,11 +1,10 @@
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb"
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
-}
+// if (!process.env.DATABASE_URL) {
+//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+// }
 
-const uri = process.env.DATABASE_URL
+const uri = process.env.DATABASE_URL || "mongodb+srv://htdrestaurant:myheartgosalalalala@restaurant.abno73r.mongodb.net/"
 const options = {}
 
 let client
